c755d551facfef551e266269b2bff1904f5e9bec
Anitha <anithachandran599@gmail.com>
2017-06-09 15:15:59 +0530

    Create repeatedvalue.java

---
+++ repeatedvalue.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class repeatedvalue
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for (int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+         for(int j=i+1;j<n;j++)
+         {
+            if(a[i]==a[j])
+            {
+                System.out.print(a[i]+" ");
+            }
+         }
+        }
+    }
+}
